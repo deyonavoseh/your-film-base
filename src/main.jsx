27027023aea543abd97b7ff39cb657fd649e82c3
Deyon Avoseh <avodeyon.07@gmail.com>
2026-01-13 08:23:29 +0100

    Fixed tailwind css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './index.css'  // ← This line must be here!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
